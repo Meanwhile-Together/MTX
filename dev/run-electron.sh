86c968198b427aba7408508b51109a53ef535604
Nackloose <nackloose@gmail.com>
2026-02-11 02:40:24 -0500

    Remove run-electron.sh script as part of project cleanup

--- dev/run-electron.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# MTX dev run-electron: run the project's desktop dev flow (Electron + nodemon etc.)
-desc="Run Electron; kill nodemon on clean exit"
-set -e
-echo "▶️ Electron desktop..." >&2
-mtx_run npm run dev:desktop
